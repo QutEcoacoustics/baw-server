64721f1df0d64de6da62a3c4975bbcd5835b40af
Mark Cottman-Fields <cofiem+git@gmail.com>
2013-12-05 10:48:11 +1000

    Added mime type video/x-ms-asf -> .asf. This caused some of the errors in #27

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -9,7 +9,7 @@ Mime::Type.register 'audio/wav', :wav, ['audio/x-wav', 'audio/vnd.wave', 'audio/
 Mime::Type.register 'audio/mp3', :mp3, ['audio/mpeg'], ['mp1', 'mp2', 'mp3', 'mpg','mpeg','mpeg3']
 Mime::Type.register 'audio/webm', :webm, ['audio/webma'], ['webma']
 Mime::Type.register 'audio/ogg', :ogg, ['audio/oga'], ['oga']
-Mime::Type.register 'audio/asf', :asf, ['audio/x-ms-asf', 'audio/x-ms-wma', 'audio/wma'], ['wma']
+Mime::Type.register 'audio/asf', :asf, ['audio/x-ms-asf', 'audio/x-ms-wma', 'audio/wma','video/x-ms-asf'], ['wma']
 Mime::Type.register 'audio/wavpack', :wv, ['audio/wv', 'audio/x-wv', 'audio/x-wavpack'], ['wavpack']
 Mime::Type.register 'audio/aac', :aac, [], []
 Mime::Type.register 'audio/mp4', :mp4, ['audio/m4a'], ['mov','m4a','3gp','3g2','mj2']
