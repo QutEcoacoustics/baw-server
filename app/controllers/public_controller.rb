9ed937345f8dad6584612729dffb988a3b9c6c6d
Mark Cottman-Fields <cofiem+git@gmail.com>
2013-12-10 16:12:30 +1000

    Work on making home page better #32 #43

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,15 +1,18 @@
 class PublicController < ApplicationController
   def index
-    respond_to do |format|
-      format.html {
-        if user_signed_in?
-          accessible_projects = current_user.recently_updated_projects
-          @projects = accessible_projects.limit(3)
+    base_path = "#{Rails.root}/public"
+    image_base = '/system/home/'
+    json_data_file = base_path+image_base+'data.js'
+    sensor_tree = base_path+image_base+'sensor_tree.jpg'
+    if File.exists?(json_data_file) && File.exists?(sensor_tree)
+      species_data = JSON.load(File.read json_data_file)
+      item_index = rand(species_data['data'].size)
+      # select a random image with audio and sensor tree
+      @selected_images = {animal: species_data['data'][item_index], sensor_tree: 'sensor_tree.jpg', image_base: image_base}
+    end
 
-        else
-          @projects = Project.none
-        end
-      }
+    respond_to do |format|
+      format.html
       format.json { no_content_as_json }
     end
   end
