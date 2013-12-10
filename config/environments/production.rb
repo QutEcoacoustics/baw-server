a0eae586035a780075e7983f09ba67b139eeec7c
Mark Cottman-Fields <cofiem+git@gmail.com>
2013-12-10 16:25:39 +1000

    trying to fix deploy error on config.paths.log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ AWB::Application.configure do
 
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  config.logger = Logger.new(config.paths.log.first, 5, 300.megabytes)
+  #config.logger = Logger.new(config.paths.log.first, 5, 300.megabytes)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
