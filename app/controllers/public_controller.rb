a5bf8819a779e687a674a93af1e3b238578735b6
Mark Cottman-Fields <cofiem+git@gmail.com>
2013-12-11 17:27:04 +1000

    Updated animal info json file.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,13 +2,14 @@ class PublicController < ApplicationController
   def index
     base_path = "#{Rails.root}/public"
     image_base = '/system/home/'
-    json_data_file = base_path+image_base+'data.js'
+    json_data_file = base_path+image_base+'animals.json'
     sensor_tree = base_path+image_base+'sensor_tree.jpg'
     if File.exists?(json_data_file) && File.exists?(sensor_tree)
       species_data = JSON.load(File.read json_data_file)
-      item_index = rand(species_data['data'].size)
+      item_count = species_data['species'].size
+      item_index = rand(item_count)
       # select a random image with audio and sensor tree
-      @selected_images = {animal: species_data['data'][item_index], sensor_tree: 'sensor_tree.jpg', image_base: image_base}
+      @selected_images = {animal: species_data['species'][item_index], sensor_tree: "#{image_base}sensor_tree.jpg", image_base: image_base+'media/'}
     end
 
     respond_to do |format|
