e2269e04986ed4bc513090ef27c3cc90c8aa6c34
Mark Cottman-Fields <cofiem+git@gmail.com>
2014-01-09 12:14:24 +1000

    Added patch to ensure numbers in json are not quoted. Closes #56 and closes #17

--- config/initializers/patches.rb
+++ config/initializers/patches.rb
@@ -71,3 +71,32 @@ module Mime
     end
   end
 end
+
+# http://stackoverflow.com/questions/6128794/rails-json-serialization-of-decimal-adds-quotes
+# patch BigDecimal so json is output without quotes.
+require 'bigdecimal'
+
+class BigDecimal
+  def as_json(options = nil) #:nodoc:
+    if finite?
+      self
+    else
+      NilClass::AS_JSON
+    end
+  end
+end
+
+# http://stackoverflow.com/questions/11743835/force-json-serialization-of-numbers-to-specific-precision/11750364#11750364
+# enable Float to support specifying the precision when converting to json
+require 'active_support/json' # gem install activesupport
+
+class Float
+  def as_json(options={})
+    if options[:decimals]
+      value = round(options[:decimals])
+      (i=value.to_i) == value ? i : value
+    else
+      super
+    end
+  end
+end
