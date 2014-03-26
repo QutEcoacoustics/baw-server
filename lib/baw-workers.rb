4a7aa0af06e20fe277ace4de80e1718db7685143
Mark Cottman-Fields <cofiem+git@gmail.com>
2014-03-26 12:32:03 +1000

    require settings class for gem to load

--- lib/baw-workers.rb
+++ lib/baw-workers.rb
@@ -4,6 +4,7 @@ require 'baw-audio-tools'
 require 'resque'
 
 require 'baw-workers/version'
+require 'baw-harvester/settings'
 
 module BawWorkers
   autoload :MediaAction, 'baw-workers/media_action'
