1badb2d31d617db75e36ab6eb8b6bfac16daee45
Mark Cottman-Fields <cofiem+git@gmail.com>
2014-04-26 22:37:12 +1000

    updated gems, changed environments to use smtp

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -36,18 +36,18 @@ AWB::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.default_url_options =
       {
           host: "#{Settings.host.name}"
       }
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.sendmail_settings =
+  config.action_mailer.smtp_settings =
       {
-          location: '/usr/sbin/sendmail',
-          arguments: '-i -t'
+          address: Settings.smtp.address
       }
 
   # Enable threaded mode
