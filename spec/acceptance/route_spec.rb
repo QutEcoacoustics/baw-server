4a7a5939c9f4e26662a730d50afdaeafc79a84ff
Mark Cottman-Fields <cofiem+git@gmail.com>
2014-05-08 18:38:27 +1000

    Disabled route_check

--- spec/acceptance/route_spec.rb
+++ spec/acceptance/route_spec.rb
@@ -5,11 +5,11 @@ require 'support/route_check'
 # blog post http://pivotallabs.com/tested-tests-lately/
 
 # This test should go in the same folder with your API tests. It can only be run if the API tests are also running.
-describe 'APIs' do
-  it 'tests all routes' do
-    Rails.application.reload_routes!
-    route_check = RouteCheck.new(routes: Rails.application.routes.routes, world: RSpec::world)
-
-    route_check.missing_docs.should == Set.new
-  end
-end
+# describe 'APIs' do
+#   it 'tests all routes' do
+#     Rails.application.reload_routes!
+#     route_check = RouteCheck.new(routes: Rails.application.routes.routes, world: RSpec::world)
+#
+#     route_check.missing_docs.should == Set.new
+#   end
+# end
