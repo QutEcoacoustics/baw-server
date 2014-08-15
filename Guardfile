1b73b43116c14e80b56e299fa87ec11eb318a7f2
cofiem <qut.bioacoustics.research+mark@gmail.com>
2014-08-15 13:15:23 +1000

    updated to latest gem versions

--- Guardfile
+++ Guardfile
@@ -1,7 +1,7 @@
 # More info at https://github.com/guard/guard#readme
 notification :off
 
-guard :rspec, cmd: 'bundle exec rspec', cli: '--format progress --color' do
+guard :rspec, cmd: 'bundle exec rspec --format progress --color' do
   watch(%r{^spec/.+_spec\.rb$})
   watch(%r{^lib/(.+)\.rb$})     { |m| "spec/#{m[1]}_spec.rb" }
   watch('spec/spec_helper.rb')  { 'spec' }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,9 +42,12 @@ require 'baw-workers'
 require File.dirname(__FILE__) + '/baw-workers/shared_context'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
