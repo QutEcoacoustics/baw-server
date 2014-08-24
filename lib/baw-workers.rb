9b71ed9274678d2a3fb19ba2dc66953d0b7aaa2a
cofiem <qut.bioacoustics.research+mark@gmail.com>
2014-08-24 20:49:33 +1000

    set Time.zone to UTC

--- lib/baw-workers.rb
+++ lib/baw-workers.rb
@@ -7,6 +7,9 @@ require 'resque_solo'
 require 'baw-workers/version'
 require 'baw-workers/settings'
 
+# set time zone
+Time.zone = 'UTC'
+
 module BawWorkers
   autoload :MediaAction, 'baw-workers/media_action'
 end
