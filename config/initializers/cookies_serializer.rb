a98f6cf1881171c2fb3aea43cc1e3278a7555912
cofiem <qut.bioacoustics.research+mark@gmail.com>
2014-11-25 15:43:03 +1000

    change cookies_serializer to :hybrid

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.action_dispatch.cookies_serializer = :json
+# @see http://guides.rubyonrails.org/upgrading_ruby_on_rails.html#cookies-serializer
+# this should be changed to :json at some point when most people have visited the site
+# and their cookies have been updated.
+Rails.application.config.action_dispatch.cookies_serializer = :hybrid
