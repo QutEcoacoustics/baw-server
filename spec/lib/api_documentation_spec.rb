323b8f2603f5da9feef840550a8bdd59a8d53628
Mark Cottman-Fields <cofiem@gmail.com>
2015-08-10 19:40:11 +1000

    got custom route check working but not filtering properly

--- spec/lib/api_documentation_spec.rb
+++ spec/lib/api_documentation_spec.rb
@@ -1,3 +1,4 @@
+## Enable this when I've figured out how to specify the routes I expect to be covered by rspec_api_documentation tests
 # require 'spec_helper'
 #
 # describe 'rspec api documentation' do
@@ -7,7 +8,8 @@
 #     Rails.application.reload_routes!
 #     route_check = Api::CustomRoutes.new(routes: Rails.application.routes.routes, world: RSpec::world)
 #
-#     route_check.missing_docs.should == Set.new
+#     missing = route_check.missing_docs
+#     expect(missing).to eq(Set.new), "expected empty set, got #{missing.size}: #{missing.map{ |m| "'#{m.to_s}'"}.join(', ')}"
 #   end
 #
 # end
