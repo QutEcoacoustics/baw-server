a95ebfb419f062a014cda7fe3ec29fb6d57e0c73
cofiem <qut.bioacoustics.research+mark@gmail.com>
2015-08-10 15:13:45 +1000

    updated schema from migration

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20150807150417) do
     t.integer  "updater_id"
     t.integer  "deleter_id"
     t.datetime "deleted_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   add_index "audio_event_comments", ["audio_event_id"], name: "index_audio_event_comments_on_audio_event_id", using: :btree
