cd737fa2392870930d7b8c18a5823e94e16cf89d
Anthony Truskinger <anthony.truskinger@gmail.com>
2016-10-04 16:40:54 +1000

    Added log filter to stop log abuse
    Recently wrote a script that allows uploading images via base64 encoded
    JSON. Default behavior saw all multi-megabyte payload dumped to logs.
    This was bad. Now only a truncated version of the data gets logged.

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,3 +2,15 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [:password]
+
+Rails.application.config.filter_parameters  << lambda do |k,v|
+  begin
+    # Bail immediately if we can
+    next unless k == 'image'
+
+    # Truncate the image data so we don't blast the logs
+    v.replace(v[0,100] + "[TRUNCATED-Length:#{v.length}]")
+  rescue Exception => e
+    Rails::logger.error e
+  end
+end
