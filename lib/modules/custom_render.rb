e009a23d1ab5b96588c9ef184afca1e678133582
Anthony Truskinger <anthony.truskinger@gmail.com>
2017-03-16 10:40:49 +1000

    Added ability to render simplified markdown
    The new `inline` parameter to the markdown custom_render.rb allows us to
    render a markdown source and then strip all block level elements. This
    is useful for embedding markdown into inline contexts (i.e. where a
    slug/descriptin/lede is needed).
    
    This was done to support #322

--- lib/modules/custom_render.rb
+++ lib/modules/custom_render.rb
@@ -1,20 +1,34 @@
 class CustomRender
   class << self
-    def render_model_markdown(model, attribute)
+
+    # @param [bool] inline - Renders markdown without HTML block elements...
+    # suitable for conversion to plainish-text strings.
+    def render_model_markdown(model, attribute, inline = false)
       value = model[attribute]
-      render_markdown(value)
+      convert(value, inline)
     end
 
+    # Intended for rendering markdown as partials
     def render_markdown(value)
+      convert(value, false)
+    end
+
+    private
+
+    def convert(value, inline)
       return nil if value.blank?
 
       # I don't know why Rubymine complains about Kramdown not being found...
-      html =  Kramdown::Document.new(
+      html = Kramdown::Document.new(
           value,
           {input: 'GFM', hard_wrap: false}
       ).to_html
 
-      ApplicationController.helpers.sanitize(html)
+      if inline
+        ApplicationController.helpers.sanitize(html, tags: ['strong', 'em'])
+      else
+        ApplicationController.helpers.sanitize(html)
+      end
     end
   end
 end
