0843d282e5bd99f1cf97ec82b29a0652a6f1e8b1
Anthony Truskinger <anthony.truskinger@gmail.com>
2020-06-02 22:56:28 +1000

    Add debug statements to entrypoint.sh

--- provision/entrypoint.sh
+++ provision/entrypoint.sh
@@ -7,11 +7,16 @@
 
 set -e
 
+echo -e "\n\n== Debug container permissions  ==\n\n"
+
+whoami
+ls -la
+
 echo -e "\n\n== Checking bundler install ==\n\n"
 
 bundle check || bundle install --system
 
-if [[ -z "${MIGRATE_DB}" ]] || ["${MIGRATE_DB,,}" == "false" ]; then
+if [[ -z "${MIGRATE_DB}" ]] || ["${MIGRATE_DB}" == "false" ]; then
   echo -e "\n\n== Skipping database status checks ==\n\n"
 else
   /home/baw_web/baw-server/provision/migrate.sh
