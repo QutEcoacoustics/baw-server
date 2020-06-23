9705f211455f8380eaf0707d5109daed4a596d28
Charles Alleman <charles@alleman.com.au>
2020-06-23 16:15:51 +1000

    Cleanup final migration edge cases

--- db/migrate/20200612004608_change_tag_notes_to_json.rb
+++ db/migrate/20200612004608_change_tag_notes_to_json.rb
@@ -2,27 +2,30 @@ class ChangeTagNotesToJson < ActiveRecord::Migration
   def change
     # https://www.citusdata.com/blog/2016/07/14/choosing-nosql-hstore-json-jsonb/
     execute <<~SQL
-      CREATE OR REPLACE FUNCTION pg_temp.is_json(varchar) RETURNS boolean AS $$
+      CREATE OR REPLACE FUNCTION pg_temp.is_json(input TEXT) RETURNS boolean AS $$
         DECLARE
-          x json;
+          data json;
         BEGIN
           BEGIN
-            x := $1;
+            data := input;
           EXCEPTION WHEN others THEN
             RETURN FALSE;
           END;
-
-          RETURN json_typeof(x) = 'object';
+          RETURN TRUE;
         END;
       $$ LANGUAGE plpgsql IMMUTABLE;
 
       ALTER TABLE "tags"
         ALTER COLUMN "notes" TYPE jsonb
         USING CASE
-          WHEN (notes <> '') is NOT TRUE THEN NULL
-          WHEN pg_temp.is_json(notes) THEN notes::json
+          WHEN (notes <> '') is NOT TRUE
+            THEN NULL
+          WHEN pg_temp.is_json(notes) AND json_typeof(notes::json) = 'object'
+            THEN notes::json
+          WHEN pg_temp.is_json(notes) AND NOT json_typeof(notes::json) = 'string'
+            THEN json_build_object('comment', notes::json)
           ELSE json_build_object('comment', notes)
-        END
+      END
     SQL
   end
 end
