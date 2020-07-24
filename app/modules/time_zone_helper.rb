c76847582ebec8712bf13ef9cc90f78505b61477
Charles Alleman <charles@alleman.com.au>
2020-07-24 13:52:47 +1000

    Models accept IANA identifier and friendly identifier
    Modified time_zone_helper.rb so that models will now accept both IANA identifiers and friendly identifiers. This was intended behaviour before, but was broken.

--- app/modules/time_zone_helper.rb
+++ app/modules/time_zone_helper.rb
@@ -15,7 +15,7 @@ class TimeZoneHelper
             offset_hours = offset / (60 * 60)
             offset_minutes = (offset % 60).to_s.rjust(2, '0')
 
-            if offset_hours < 0
+            if offset_hours.negative?
               offset_hours = (offset_hours * -1).to_s.rjust(2, '0')
               offset_hours = '-' + offset_hours
             else
@@ -30,14 +30,24 @@ class TimeZoneHelper
       ]
     end
 
-    def to_identifier(friendly_name)
-      found = TZInfo::Timezone.all.select { |tz| friendly_name == tz.friendly_identifier }
-      found[0].identifier if found.size == 1
+    # Find a Ruby TimeZone which matches the identifier
+    # @param [string] identifier Ruby TimeZone identifier or friendly identifier
+    def find_timezone(identifier)
+      TZInfo::Timezone.all.find { |tz| [tz.friendly_identifier, tz.identifier].include? identifier }
     end
 
+    # Retrieve the official identifier of a Ruby TimeZone
+    # @param [string] identifier Ruby TimeZone identifier or friendly identifier
+    def to_identifier(identifier)
+      timezone = find_timezone(identifier)
+      timezone.identifier unless timezone.blank?
+    end
+
+    # Retrieve the friendly identifier of a Ruby TimeZone
+    # @param [string] identifier Ruby TimeZone identifier or friendly identifier
     def to_friendly(identifier)
-      found = TZInfo::Timezone.all.select { |tz| identifier == tz.identifier }
-      found[0].friendly_identifier if found.size == 1
+      timezone = find_timezone(identifier)
+      timezone.friendly_identifier unless timezone.blank?
     end
 
     # Get the Ruby TimeZone for the name.
@@ -73,13 +83,14 @@ class TimeZoneHelper
     # @return [Boolean]
     def validate_tzinfo_tz(model)
       tzinfo_friendly = model.tzinfo_tz
-      is_invalid = to_identifier(tzinfo_friendly).blank?
-      if !model.tzinfo_tz.blank? && is_invalid
-        suggestions = tzinfo_friendly_did_you_mean(tzinfo_friendly)[0..2]
-        msg1 = "is not a recognised timezone ('#{tzinfo_friendly}'"
-        msg2 = suggestions.any? ? " - did you mean '#{suggestions.join("', '")}'?)" : ')'
-        model.errors[:tzinfo_tz] << msg1 + msg2
-      end
+      is_invalid = find_timezone(tzinfo_friendly).blank?
+
+      return unless !model.tzinfo_tz.blank? && is_invalid
+
+      suggestions = tzinfo_friendly_did_you_mean(tzinfo_friendly)[0..2]
+      msg1 = "is not a recognized timezone ('#{tzinfo_friendly}'"
+      msg2 = suggestions.any? ? " - did you mean '#{suggestions.join("', '")}'?)" : ')'
+      model.errors[:tzinfo_tz] << msg1 + msg2
     end
 
     # Get suggestions for a tzinfo friendly name.
@@ -88,7 +99,7 @@ class TimeZoneHelper
     def tzinfo_friendly_did_you_mean(tzinfo_friendly)
       matches = []
       TZInfo::Timezone.all.each do |tz|
-        matches << tz.friendly_identifier if tz.friendly_identifier.include?(tzinfo_friendly)
+        matches << tz.friendly_identifier if [tz.friendly_identifier, tz.identifier].include? tzinfo_friendly
       end
       matches
     end
@@ -107,34 +118,36 @@ class TimeZoneHelper
     end
 
     def offset_seconds_to_formatted(utc_offset_seconds)
-      is_neg = utc_offset_seconds < 0
+      is_neg = utc_offset_seconds.negative?
       sec = utc_offset_seconds.abs
       hours = (sec / (60 * 60)).floor.to_s.rjust(2, '0')
       minutes = ((sec % (60 * 60)) / 60).floor.to_s.rjust(2, '0')
       "#{is_neg ? '-' : '+'}#{hours}:#{minutes}"
     end
 
+    # Attempt to set the Ruby TimeZone identifier for a model
+    # @param [ApplicationRecord] model
+    # @returns [String] Ruby TimeZone identifier
+    def update_model_timezone(model)
+      timezone = TimeZoneHelper.find_timezone(model.tzinfo_tz)
+      return nil if timezone.blank?
+
+      # update the column to friendly identifier for consistency
+      # save only this column, skip validation, does not skip callbacks
+      model.update_attribute(:tzinfo_tz, timezone.friendly_identifier)
+      timezone.identifier
+    end
+
     # Parse (and if needed repairs) timezone information from one of our models.
     # Handles wrong timezone format stored and invalid timezone formats.
     def parse_model(model)
-      # the model stores the tzinfo friendly_identifier
-      tzinfo_tz_string = TimeZoneHelper.to_identifier(model.tzinfo_tz)
-      # attempt to parse the identifier form as a backup
-      if tzinfo_tz_string.blank?
-        friendly = TimeZoneHelper.to_friendly(model.tzinfo_tz)
-        unless friendly.blank?
-          tzinfo_tz_string = model.tzinfo_tz
-          # update the column to friendly identifier for consistency
-          # save only this column, skip validation, does not skip callbacks
-          model.update_attribute(:tzinfo_tz, friendly)
-        end
-      end
+      tzinfo_tz_string = update_model_timezone(model)
       tzinfo_tz = tzinfo_class(tzinfo_tz_string)
 
       rails_tz_string = model.rails_tz
       rails_tz = ruby_tz_class(rails_tz_string)
 
-      # if neither tz can be determined, but either model value is set, it is likely that the timezone is corrupt.
+      # If neither tz can be determined, but either model value is set, it is likely that the timezone is corrupt.
       # Reset to nil.
       if (tzinfo_tz.nil? && rails_tz.nil?) && (!model.tzinfo_tz.blank? || !model.rails_tz.blank?)
         # save only those two columns, bypass validations, callbacks, and updated_at
@@ -152,15 +165,15 @@ class TimeZoneHelper
       tzinfo_tz = ruby_to_tzinfo(rails_tz_string) if !rails_tz.blank? && tzinfo_tz.blank?
       rails_tz = tzinfo_to_ruby(tzinfo_tz_string) if rails_tz.blank? && !tzinfo_tz.blank?
 
-      if tzinfo_tz
-        {
-          identifier_alt: rails_tz.blank? ? nil : rails_tz.name,
-          identifier: tzinfo_tz.identifier,
-          friendly_identifier: tzinfo_tz.friendly_identifier,
-          utc_offset: rails_tz.blank? ? tzinfo_tz.current_period.utc_offset : rails_tz.utc_offset,
-          utc_total_offset: tzinfo_tz.current_period.utc_total_offset
-        }
-      end
+      return unless tzinfo_tz
+
+      {
+        identifier_alt: rails_tz.blank? ? nil : rails_tz.name,
+        identifier: tzinfo_tz.identifier,
+        friendly_identifier: tzinfo_tz.friendly_identifier,
+        utc_offset: rails_tz.blank? ? tzinfo_tz.current_period.utc_offset : rails_tz.utc_offset,
+        utc_total_offset: tzinfo_tz.current_period.utc_total_offset
+      }
     end
   end
 end
