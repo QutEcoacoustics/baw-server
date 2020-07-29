a96f568dbecd8a967e8e92857d17ebbc4dea1d75
Anthony Truskinger <anthony.truskinger@gmail.com>
2020-07-29 12:26:40 +1000

    Convert multi assertion test to single assertion
    Addresses feedback from https://github.com/QutEcoacoustics/baw-server/commit/7b5aeda31d4dea44222f0ffce2b9e8f6e72aa6e9#r40989239

--- spec/unit/modules/time_zone_helper_spec.rb
+++ spec/unit/modules/time_zone_helper_spec.rb
@@ -101,10 +101,13 @@ describe TimeZoneHelper do
     )
   end
 
-  it 'can convert an offset in seconds into a readable timestamp' do
-    [[-7200, '-02:00'], [0, '+00:00'], [36_000, '+10:00']].each do |seconds, expected|
-      actual = TimeZoneHelper.offset_seconds_to_formatted(seconds)
-      expect(actual).to eq(expected)
+  context 'can convert an offset in seconds into a readable timestamp' do
+    CASES = [[-7200, '-02:00'], [0, '+00:00'], [15_300, '+04:15'], [36_000, '+10:00']]
+    CASES.each do |seconds, expected|
+      example "#{seconds} ➡ #{expected}" do
+        actual = TimeZoneHelper.offset_seconds_to_formatted(seconds)
+        expect(actual).to eq(expected)
+      end
     end
   end
 
