5c8969c84edc8c7e09a5d4afeedef0b021e91260
Anthony Truskinger <anthony.truskinger@gmail.com>
2020-09-28 06:10:10 +0000

    Cleaned up OpenAPI mounted engine tests

--- spec/requests/api_docs.rb
+++ spec/requests/api_docs.rb
@@ -3,16 +3,21 @@
 require 'rails_helper'
 
 describe 'api-docs' do
-  it 'is mounted at api_docs' do
-    get '/api-docs'
+  it 'the OpenAPI app is mounted at api_docs' do
+    request = get '/api-docs'
+    expect(request).to redirect_to('/api-docs/index.html')
   end
 
-  it 'changes default host and scheme for localhost' do
+  def expect_openapi_doc
     get '/api-docs/v2/swagger.yaml'
 
     expect_success
     expect(response.content_type).to eq('text/yaml')
-    result = YAML.safe_load(response_body, symbolize_names: true)
+    YAML.safe_load(response_body, symbolize_names: true)
+  end
+
+  it 'can return the OpenAPI document' do
+    result = expect_openapi_doc
     expect(result).to match(hash_including({
       openapi: an_instance_of(String),
       info: {
@@ -22,6 +27,14 @@ describe 'api-docs' do
       produces: an_instance_of(Array),
       consumes: an_instance_of(Array),
       paths: an_instance_of(Hash),
+      servers: an_instance_of(Array),
+      components: an_instance_of(Hash)
+    }))
+  end
+
+  it 'changes default host and scheme for localhost' do
+    result = expect_openapi_doc
+    expect(result).to match(hash_including({
       servers: [
         {
           url: '{protocol}://{authority}',
@@ -35,30 +48,17 @@ describe 'api-docs' do
             }
           }
         }
-      ],
-      components: an_instance_of(Hash)
+      ]
     }))
   end
 
-  it '/changes default host and scheme for other hosts' do
+  it 'changes default host and scheme for other hosts' do
     allow(Settings.host).to receive(:name).and_return('www.ecosounds.org')
     allow(Settings.host).to receive(:port).and_return('')
     allow(BawApp).to receive(:dev_or_test?).and_return(false)
 
-    get '/api-docs/v2/swagger.yaml'
-
-    expect_success
-    expect(response.content_type).to eq('text/yaml')
-    result = YAML.safe_load(response_body, symbolize_names: true)
+    result = expect_openapi_doc
     expect(result).to match(hash_including({
-      openapi: an_instance_of(String),
-      info: {
-        title: 'Acoustic Workbench API',
-        version: 'v2'
-      },
-      produces: an_instance_of(Array),
-      consumes: an_instance_of(Array),
-      paths: an_instance_of(Hash),
       servers: [
         {
           url: '{protocol}://{authority}',
@@ -72,8 +72,7 @@ describe 'api-docs' do
             }
           }
         }
-      ],
-      components: an_instance_of(Hash)
+      ]
     }))
   end
 end
