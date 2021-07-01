1331f59b9dea3a68e815efd8ed6cc30bcd061f6b
Anthony Truskinger <anthony.truskinger@gmail.com>
2021-07-01 05:18:10 +0000

    dev: persist command history in vscode containers

--- provision/dev_setup.sh
+++ provision/dev_setup.sh
@@ -7,4 +7,10 @@ curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.s
 DEBIAN_FRONTEND=noninteractive apt-get install git-lfs
 git lfs install
 
-
+USERNAME=baw_web
+SNIPPET="export PROMPT_COMMAND='history -a' && export HISTFILE=/commandhistory/.bash_history" \
+    && mkdir -p /commandhistory \
+    && chmod 777 /commandhistory \
+    && touch /commandhistory/.bash_history \
+    && chown -R $USERNAME /commandhistory \
+    && echo $SNIPPET >> "/home/$USERNAME/.bashrc"
