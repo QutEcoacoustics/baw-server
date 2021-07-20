fa5eb9c43051866d538d06cf1abcbb656e5f5d97
Anthony Truskinger <anthony.truskinger@gmail.com>
2021-07-20 05:21:52 +0000

    Chore: ensure .bashrc available in dev container

--- provision/dev_setup.sh
+++ provision/dev_setup.sh
@@ -8,6 +8,11 @@ DEBIAN_FRONTEND=noninteractive apt-get install git-lfs
 git lfs install
 
 USERNAME=baw_web
+
+# docker container now missing .bashrc by default. Useful for interactive situations
+/bin/cp /etc/skel/.bashrc "/home/$USERNAME/.bashrc"
+chown $USERNAME "/home/$USERNAME/.bashrc"
+
 SNIPPET="export PROMPT_COMMAND='history -a' && export HISTFILE=/commandhistory/.bash_history" \
     && mkdir -p /commandhistory \
     && chmod 777 /commandhistory \
