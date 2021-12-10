QutEcoacoustics/baw-server

commit 560652b782e051192d24aa2afa56c285737d8932

Fixes various thing
- Now allows custom fields (of the new variant) to be used in projections, as well as field mappings, and associations. Fixes #495
- converts site's custom_latitutde and custom_longitude fields to new custom_fields2 so they can be used in projections
- Also simplified Site's implementation of jittering - we no longer mask the original attributes, rather we only serialize the custom_* variants
- added canonical_file_name and recorded_date_timezone to AudioRecording as custom_fields2
- added region to the associations list of an audio_recording. This filters can use `regions.id` as a filter or projection
- used canonical_file_name and site.name in filter projection  to improve downloader script - we no longer need to construct a filename in the pwhs script or fetch the site name as a separate request for each recording
- Removed `-Resume` from download options for downloader - it was not working properly, only downloading first 512KB
- a side effect of these changes results in `respond_new` and `new_spec_fields` becoming requried - simplifies code in the response.rb file
- implemented the Current class; this is Rails' official method of providing request context to call sites that can't access that context - e.g. Current.user where current_user is not available.
- Added support for rdbg as a default debugger in vscode settings

app/modules/api/response.rb
@@ -26,12 +26,12 @@ module Api
       Rack::Utils::HTTP_STATUS_CODES[status_code(status_symbol)]
     end
 
-    # Add custom fields to an item.
+    # Formats an item for a #new response.
     # @param [Object] item A single item from the response.
     # @param [Object] user current_user
     # @param [Hash] opts the options for additional information.
     # @return [Hash] prepared item
-    def prepare(item, user, opts = {})
+    def prepare_new(item, user, _opts = {})
       unless item.is_a?(ActiveRecord::Base)
         raise CustomErrors::FilterArgumentError, "Item must be an ActiveRecord::Base, got #{item.class}"
       end
@@ -47,25 +47,72 @@ module Api
         new_spec_fields_hash = new_spec_fields.call(user)
       end
 
+      new_spec_fields_hash
+    end
+
+    # Add custom fields to an item.
+    # @param [Object] item A single item from the response.
+    # @param [Object] user current_user
+    # @param [Hash] opts the options for additional information.
+    # @return [Hash] prepared item
+    def prepare(item, user, opts = {})
+      unless item.is_a?(ActiveRecord::Base)
+        raise CustomErrors::FilterArgumentError, "Item must be an ActiveRecord::Base, got #{item.class}"
+      end
+
+      filter_settings = item.class.filter_settings
+      item_new = item
+
       # add custom fields if filter_settings specifies a lambda for custom_fields
       custom_fields = filter_settings[:custom_fields]
       custom_fields_is_lambda = !custom_fields.blank? && custom_fields.lambda?
       custom_fields_hash = {}
       if custom_fields_is_lambda && !item_new.nil? && !item_new.id.nil?
         item_new, custom_fields_hash = custom_fields.call(item, user)
+        custom_fields_hash.transform_keys!(&:to_s)
       end
+      custom_fields_keys = custom_fields_hash.keys
 
-      # project using filter projection (already in query for items) or default fields
-      has_projection = opts[:projection]
-      item_new = {} if item_new.nil?
-      if has_projection
-        base_json = item_new.as_json
+      # get newer sort of custom fields, and keep only string keys and transforms
+      custom_fields_hash2 = filter_settings
+                            .fetch(:custom_fields2, {})
+                            .transform_values { |value| value[:transform] }
+                            .transform_keys(&:to_s)
+
+      hashed_item = {}.merge(
+        item_new&.as_json,
+        custom_fields_hash,
+        custom_fields_hash2
+      )
+
+      # project using filter projection or default fields
+      # Note: most queries with a projection already only return required fields
+      # but some don't... currently those using custom_filter_2
+      projection = opts[:projection]
+      if projection
+        if projection[:include]
+          # backwards compatible hack: custom fields always used to be included,
+          # no matter the projection
+          hashed_item.slice(*(projection[:include] + custom_fields_keys))
+        else
+          hashed_item.except(*projection[:exclude])
+        end => hashed_item
       else
-        default_fields = filter_settings[:render_fields]
-        base_json = item_new.as_json(only: default_fields)
+        default_fields = filter_settings[:render_fields] + custom_fields_keys
+        hashed_item = hashed_item.slice(*default_fields.map(&:to_s))
       end
 
-      base_json.merge(new_spec_fields_hash).merge(custom_fields_hash)
+      # Now that the projection is applied, transform any remaining
+      # custom values.
+      # We don't want to do this earlier because calculating a custom field
+      # before the projection could result in unneeded custom fields being
+      # included which apart from being slow may also fail due to missing select
+      # data
+      hashed_item.transform_values { |value|
+        next value unless value.is_a?(Proc)
+
+        value.call(item)
+      }
     end
 
     # Build an api response hash.
@@ -146,7 +193,7 @@ module Api
         items = opts[:items].to_f
         total = opts[:total].to_f
         # prevent divide by 0 error
-        max_page = items > 0 ? (total / items).ceil : 1
+        max_page = items.positive? ? (total / items).ceil : 1
         opts[:max_page] = max_page
         result[:meta][:paging][:max_page] = max_page
       end
@@ -343,7 +390,7 @@ module Api
     def to_f_or_i_or_s(v)
       # http://stackoverflow.com/questions/8071533/convert-input-value-to-integer-or-float-as-appropriate-using-ruby
 
-      ((float = Float(v)) && (float % 1.0 == 0) ? float.to_i : float)
+      ((float = Float(v)) && (float % 1.0).zero? ? float.to_i : float)
     rescue StandardError
       v
     end
@@ -388,7 +435,7 @@ module Api
 
       unless filter_generic_keys.blank?
         filter_generic_keys.each do |key, value|
-          link_params[('filter_' + key.to_s).to_sym] = value
+          link_params["filter_#{key}".to_sym] = value
         end
       end
 

app/modules/filter/build.rb
@@ -26,6 +26,7 @@ module Filter
       @base_association = filter_settings[:base_association]
       @valid_associations = filter_settings[:valid_associations]
       @field_mappings = filter_settings[:field_mappings]
+      @custom_fields2 = filter_settings[:custom_fields2] || {}
 
       @valid_conditions = [
         # comparison
@@ -102,9 +103,14 @@ module Filter
       end
 
       # create projection that includes each column
+
       columns.map { |item|
-        project_column(@table, item, @render_fields)
-      }
+        project_column(@table, item, allowed_fields)
+      }.flatten.compact
+    end
+
+    def allowed_fields
+      (@render_fields + @custom_fields2.keys).uniq
     end
 
     # Combine two conditions.
@@ -494,9 +500,7 @@ module Filter
       validate_table(table)
       raise CustomErrors::FilterArgumentError, 'Field name must be a symbol.' unless field.is_a?(Symbol)
 
-      field_s = field.to_s
-
-      if field_s.include?('.')
+      if association_field?(field)
         parse_other_table_field(table, field)
       else
 
@@ -512,6 +516,10 @@ module Filter
           filter_settings: @filter_settings
         }
       end
+    end
+
+    def association_field?(name)
+      name.to_s.include?('.')
     end
 
     # Build other table field from field symbol.

app/modules/filter/query.rb
@@ -12,9 +12,10 @@ module Filter
     include Custom
 
     attr_reader :key_prefix, :max_items, :initial_query, :table,
-                :valid_fields, :text_fields, :filter_settings,
-                :parameters, :filter, :projection, :qsp_text_filter,
-                :qsp_generic_filters, :paging, :sorting, :build
+      :valid_fields, :text_fields, :filter_settings,
+      :parameters, :filter, :projection, :qsp_text_filter,
+      :qsp_generic_filters, :paging, :sorting, :build,
+      :custom_fields2
 
     # Convert a json POST body to an arel query.
     # @param [Hash] parameters
@@ -35,12 +36,10 @@ module Filter
       @initial_query = !query.nil? && query.is_a?(ActiveRecord::Relation) ? query : relation_all(model)
       # the check for an active record relation above can lead to some subtle
       # bugs when you pass some non-nil query like thing.
-      # temporarily throwing here to see what breaks. If it is nothing too severe
-      # then we'll make the check below a validation that always rungs
-      unless Rails.env.production?
-        unless query.is_a?(ActiveRecord::Relation)
-          raise ArgumentError, "query was not an ActiveRecord::Relation. Query: #{query}"
-        end
+      # temporarily throwing here to see what breaks. All tests pass so
+      # we'll make the check below a validation that always runs
+      unless query.is_a?(ActiveRecord::Relation)
+        raise ArgumentError, "query was not an ActiveRecord::Relation. Query: #{query}"
       end
 
       validate_filter_settings(filter_settings)
@@ -50,6 +49,7 @@ module Filter
       @filter_settings = filter_settings
       @default_sort_order = filter_settings[:defaults][:order_by]
       @default_sort_direction = filter_settings[:defaults][:direction]
+      @custom_fields2 = filter_settings[:custom_fields2] || {}
 
       @build = Build.new(@table, filter_settings)
 
@@ -148,14 +148,6 @@ module Filter
       else
         query_projection_default(query)
       end
-    end
-
-    # Add projections to a query.
-    # @param [ActiveRecord::Relation] query
-    # @param [Array<Symbol>] filter_projection
-    # @return [ActiveRecord::Relation] query
-    def query_projection_custom(query, filter_projection)
-      apply_projections(query, @build.projections({ include: filter_projection }))
     end
 
     # Add default projections to a query.
@@ -319,14 +311,12 @@ module Filter
 
       sort_field_by = if sort_field.is_a? String
                         sort_field
-                      else
+                      elsif direction == :desc
 
-                        if direction == :desc
-                          Arel::Nodes::Descending.new(sort_field)
-                        else
-                          #direction == :asc
-                          Arel::Nodes::Ascending.new(sort_field)
-                                        end
+                        Arel::Nodes::Descending.new(sort_field)
+                      else
+                        #direction == :asc
+                        Arel::Nodes::Ascending.new(sort_field)
 
                       end
 
@@ -362,6 +352,18 @@ module Filter
     def apply_projection(query, projection)
       validate_query(query)
       validate_projection(projection)
+
+      if projection.is_a?(Hash)
+        expression = projection[:base_table]
+        projection[:joins].each do |join|
+          expression = expression.join(join[:arel_table], join[:type]).on(join[:on])
+        end
+        query = query.joins(expression.join_sources)
+        query = query.select(projection[:projection])
+
+        return query
+      end
+
       query.select(projection)
     end
   end

spec/requests/audio_recordings_spec.rb
@@ -8,8 +8,9 @@ describe '/audio_recordings' do
       site.tzinfo_tz = 'Australia/Sydney'
       site.save!
 
-      get "/audio_recordings/#{audio_recording.id}", headers: api_request_headers(reader_token)
-      expect(response).to have_http_status(:success)
+      get "/audio_recordings/#{audio_recording.id}", **api_headers(reader_token)
+
+      expect_success
       expect(api_result).to include(data: hash_including({
         recorded_date_timezone: 'Australia/Sydney'
       }))
@@ -19,11 +20,84 @@ describe '/audio_recordings' do
       site.tzinfo_tz = nil
       site.save!
 
-      get "/audio_recordings/#{audio_recording.id}", headers: api_request_headers(reader_token)
-      expect(response).to have_http_status(:success)
+      get "/audio_recordings/#{audio_recording.id}", **api_headers(reader_token)
+
+      expect_success
       expect(api_result).to include(data: hash_including({
         recorded_date_timezone: nil
       }))
     end
+  end
+
+  example 'returns a canonical_name' do
+    site.tzinfo_tz = 'Australia/Brisbane'
+    site.save!
+
+    get "/audio_recordings/#{audio_recording.id}", **api_headers(reader_token)
+
+    expect_success
+    expect(api_data).to include(
+      canonical_file_name: audio_recording.friendly_name
+    )
+  end
+
+  example 'returns a canonical_name only when requested' do
+    site.tzinfo_tz = 'Australia/Brisbane'
+    site.save!
+
+    body = {
+      projection: { include: [:canonical_file_name] },
+      filter: { id: { eq: audio_recording.id } }
+    }
+
+    post '/audio_recordings/filter', params: body, **api_with_body_headers(reader_token)
+
+    expect_success
+    expect_number_of_items(1)
+
+    expect(api_data).to eq([{
+      canonical_file_name: audio_recording.friendly_name
+    }])
+  end
+
+  example 'can return only the information needed for a task like downloading when requested' do
+    site.tzinfo_tz = 'Australia/Brisbane'
+    site.save!
+
+    body = {
+      projection: { include: [:id, :recorded_date, :'sites.name', :site_id, :canonical_file_name] },
+      filter: { id: { eq: audio_recording.id } }
+    }
+
+    post '/audio_recordings/filter', params: body, **api_with_body_headers(reader_token)
+
+    expect_success
+    expect_number_of_items(1)
+
+    expect(api_data).to match([{
+      canonical_file_name: audio_recording.friendly_name,
+      id: audio_recording.id,
+      site_id: audio_recording.site_id,
+      'sites.name': audio_recording.site.name,
+      recorded_date: an_instance_of(String)
+    }])
+  end
+
+  example 'can filter by regions.id' do
+    body = {
+      projection: { include: [:id, :'regions.name', :'regions.id'] },
+      filter: { 'regions.id': { eq: region.id } }
+    }
+
+    post '/audio_recordings/filter', params: body, **api_with_body_headers(reader_token)
+
+    expect_success
+    expect_number_of_items(region.sites.collect(&:audio_recordings).count)
+
+    expect(api_data).to match([{
+      id: audio_recording.id,
+      'regions.id': audio_recording.site.region.id,
+      'regions.name': audio_recording.site.region.name
+    }])
   end
 end

Gemfile
@@ -208,6 +208,7 @@ end
 group :development do
   # allow debugging
   #gem 'debase', '>= 0.2.5.beta2'
+  gem 'debug', ">= 1.0.0"
   gem 'readapt'
   #gem 'ruby-debug-ide', '>= 0.7.2'
   #gem 'traceroute'

app/controllers/analysis_jobs_controller.rb
@@ -34,7 +34,7 @@ class AnalysisJobsController < ApplicationController
     do_set_attributes
     do_authorize_instance
 
-    respond_show
+    respond_new
   end
 
   # POST /analysis_jobs

app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include IncludeController
 
   include ActiveStorage::SetCurrent
+  include SetCurrent
 
   layout :select_layout
 

app/controllers/audio_event_comments_controller.rb
@@ -34,7 +34,7 @@ class AudioEventCommentsController < ApplicationController
     do_set_attributes
     do_authorize_instance
 
-    respond_show
+    respond_new
   end
 
   # POST /audio_events/:audio_event_id/comments

commit edd1f8f4d3e2a240ede2f5e3acf29acd7cd9748e

Fixes projection case normalization
I didn't write any tests for projections that used keys in another case. Our API theoretically supports field names in either snake_case or camelCase but we don't often think to test it since camelCase is so rarely used.

Bug introduced in 560652b782e051192d24aa2afa56c285737d8932.

Fixed by ensuring projection keys are normalized in a spot where they can be shared with later parts of the filtering pipeline. In this case, our second-pass projection include/exclude mechanism needed the updated column names.

Fixes #550

app/modules/api/response.rb
@@ -93,9 +93,9 @@ module Api
         if projection[:include]
           # backwards compatible hack: custom fields always used to be included,
           # no matter the projection
-          hashed_item.slice(*(projection[:include] + custom_fields_keys))
+          hashed_item.slice(*(projection[:include].map(&:to_s) + custom_fields_keys))
         else
-          hashed_item.except(*projection[:exclude])
+          hashed_item.except(*projection[:exclude].map(&:to_s))
         end => hashed_item
       else
         default_fields = filter_settings[:render_fields] + custom_fields_keys

app/modules/filter/build.rb
@@ -92,14 +92,14 @@ module Filter
       when :include
         raise CustomErrors::FilterArgumentError, 'Include must contain at least one field.' if value.blank?
 
-        columns = value.map { |x| CleanParams.clean(x) }
+        columns = value
       when :exclude
         raise CustomErrors::FilterArgumentError, 'Exclude must contain at least one field.' if value.blank?
 
-        columns = @render_fields.reject { |item| value.include?(item) }.map { |x| CleanParams.clean(x) }
+        columns = @render_fields.reject { |item| value.include?(item) }
         raise CustomErrors::FilterArgumentError, 'Exclude must contain at least one field.' if columns.blank?
       else
-        raise CustomErrors::FilterArgumentError, "Unrecognised projection key #{key}."
+        raise CustomErrors::FilterArgumentError, "Unrecognized projection key #{key}."
       end
 
       # create projection that includes each column

app/modules/filter/parse.rb
@@ -54,15 +54,12 @@ module Filter
 
       # ensure items is always less than max_items
       if items > max_items
-        raise CustomErrors::UnprocessableEntityError, "Number of items per page requested #{items} exceeded maximum #{max_items}."
+        raise CustomErrors::UnprocessableEntityError,
+          "Number of items per page requested #{items} exceeded maximum #{max_items}."
       end
 
       # parse disable paging settings
-      disable_paging = if disable_paging == 'true' || disable_paging == true
-                         true
-                       else
-                         false
-                       end
+      disable_paging = ['true', true].include?(disable_paging)
 
       # calculate offset and limit
       offset = (page - 1) * items
@@ -97,6 +94,21 @@ module Filter
       { order_by: order_by, direction: direction }
     end
 
+    def parse_projection(params)
+      return nil unless params.include?(:projection) && !params[:projection].blank?
+
+      projection = params[:projection]
+      case projection
+      in include: Array => i
+        { include: i.map { |x| CleanParams.clean(x) } }
+      in exclude: Array => e
+        { exclude: e.map { |x| CleanParams.clean(x) } }
+      else
+        # further validation done is #projections - we're just normalizing values here
+        projection
+      end
+    end
+
     # Parse text from parameters.
     # Any query string parameter will override filters already present.
     # @param [Hash] params
@@ -124,9 +136,10 @@ module Filter
     # @param [Hash] found
     # @return [Hash] matching entries
     def parse_qsp(obj, value, key_prefix, found = {})
-      if value.is_a?(Hash)
+      case value
+      when Hash
         found = parse_qsp_hash(value, key_prefix, found)
-      elsif value.is_a?(Array)
+      when Array
         found = parse_qsp_array(obj, value, key_prefix, found)
       else
         key_s = obj.blank? ? '' : obj.to_s

app/modules/filter/query.rb
@@ -57,7 +57,7 @@ module Filter
       validate_hash(@parameters)
 
       @filter = @parameters.include?(:filter) && !@parameters[:filter].blank? ? @parameters[:filter] : {}
-      @projection = @parameters.include?(:projection) && !@parameters[:projection].blank? ? @parameters[:projection] : nil
+      @projection = parse_projection(@parameters)
 
       # remove key_partial_match key from parameters hash
       parameters_for_generic = @parameters.dup

spec/requests/audio_recordings_spec.rb
@@ -100,4 +100,46 @@ describe '/audio_recordings' do
       'regions.name': audio_recording.site.region.name
     }])
   end
+
+  example 'it can retrieve recent audio recordings' do
+    body = {
+      projection: { include: [:id, :siteId, :durationSeconds, :recordedDate, :createdAt] },
+      sorting: { orderBy: 'createdAt', direction: 'desc' }
+    }
+
+    post '/audio_recordings/filter', params: body, **api_with_body_headers(reader_token)
+
+    expect_success
+    expect_number_of_items(AudioRecording.count)
+
+    expect(api_data).to match([{
+      id: audio_recording.id,
+      site_id: audio_recording.site_id,
+      duration_seconds: audio_recording.duration_seconds,
+      recorded_date: audio_recording.recorded_date,
+      created_at: audio_recording.created_at.iso8601(3)
+    }])
+  end
+
+  example 'it can retrieve audio recordings entries for visualize' do
+    body = {
+      filter: { siteId: { in: [audio_recording.site_id] } },
+      paging: { disablePaging: true },
+      projection: { include: ['id', 'uuid', 'siteId', 'durationSeconds', 'recordedDate'] },
+      sorting: { orderBy: 'id', direction: 'asc' }
+    }
+
+    post '/audio_recordings/filter', params: body, **api_with_body_headers(reader_token)
+
+    expect_success
+    expect_number_of_items(AudioRecording.count)
+
+    expect(api_data).to match([{
+      id: audio_recording.id,
+      uuid: audio_recording.uuid,
+      site_id: audio_recording.site_id,
+      duration_seconds: audio_recording.duration_seconds,
+      recorded_date: audio_recording.recorded_date
+    }])
+  end
 end