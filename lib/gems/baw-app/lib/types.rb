QutEcoacoustics/baw-server

commit 1690c672c0f1f5efbde2fefc2de9447185fe299b

Makes auth token expire only if not used after a while (#730)
We changed auth tokens so they would expire if a new session was created - as they were always meant to - in eedfd00c4b4de09d765e117101507e589f0ee589.

This of course broke everyone who depended on the tokens never expiring - particularly for API based programs.

Until we can provide an alternative (see Allow users to mint JWTs for scripts #727) we've made tokens stable _while they continue to be used_. Any session interaction will prolong the life of the token. This should make it so anyone using a token regularly - e.g. for scripted access - will be able to do so until their activity stops.

Fixes Token expiration too harsh #728

* Address review feedback

config/settings/default.yml
@@ -171,6 +171,10 @@ devise:
 ######################################################
 ##secret_token: some long secret token - at least 30 chars, all random
 ##secret_key_base: some long secret token - at least 30 chars, all random
+authentication:
+#   # how long a user can be inactive before their token expires
+#   # Any auth method will refresh the expiration time.
+   token_rolling_expiration: 86400
 paths:
   original_audios:
     - /data/<%= BawApp.env %>/original_audio

lib/gems/baw-app/lib/types.rb
@@ -91,6 +91,12 @@ module BawApp
       ::BawApp::Sexagesimal.parse(value)
     }
 
+    Duration = Constructor(ActiveSupport::Duration) { |value|
+      next nil if value.blank?
+
+      ActiveSupport::Duration.seconds(value.to_i_strict)
+    }
+
     PbsByteFormat = Constructor(Numeric) { |value|
       next nil if value.blank?
 

app/models/user.rb
@@ -323,14 +323,10 @@ class User < ApplicationRecord
     end
   end
 
-  # @see http://stackoverflow.com/a/19071745/31567
-  def self.find_by_authentication_token(authentication_token = nil)
-    where(authentication_token:).first if authentication_token
-  end
-
   def after_database_authentication
-    # reset user token on valid sign in with username/password
-    reset_authentication_token!
+    # Generate a new authentication token if the current one is expired.
+    # We're only here after a successful login from other means.
+    reset_authentication_token! if expired_authentication_token?
   end
 
   def ensure_authentication_token
@@ -347,6 +343,18 @@ class User < ApplicationRecord
   def clear_authentication_token!
     self.authentication_token = nil
     save!
+  end
+
+  def expired_authentication_token?
+    return true if authentication_token.blank?
+
+    last_activity = [current_sign_in_at, last_sign_in_at, last_seen_at].compact.max
+
+    # we can have no activity for brand new users, in which case the token is not expired
+    return false if last_activity.nil?
+
+    time_since = Time.zone.now - last_activity
+    time_since > Settings.authentication.token_rolling_expiration
   end
 
   # Store the current_user id in the thread so it can be accessed by models

app/modules/api/auth_strategies/jwt.rb
@@ -9,6 +9,8 @@ module Api
       ENV_KEY = :api_auth_strategies_jwt
       HEADER = 'Authorization'
       HEADER_FORMAT = /Bearer ([-_.A-Za-z0-9]+)/
+      HEADER_KEY = /Bearer/i
+      INCORRECT_FORMAT = 'Incorrect bearer format'
 
       def valid?
         !parse_token.nil?
@@ -17,7 +19,7 @@ module Api
       def authenticate!
         token = parse_token
 
-        return if token.nil?
+        return token unless token.is_a?(String)
 
         jwt = ::Api::Jwt.decode(token)
         unless jwt.valid?
@@ -41,7 +43,7 @@ module Api
 
       protected
 
-      # @return [String,nil] the bearer token if valid, or nil if the header is not
+      # @return [String,nil,Symbol] the bearer token if valid, or nil if the header is not
       def parse_token
         header = authorization_header
 
@@ -54,14 +56,19 @@ module Api
         request.headers['Authorization']&.to_s
       end
 
-      # @return [String,nil] the bearer token if valid, or nil if the header is not
+      # @return [String,nil,Symbol]
+      #   the bearer token if valid,
+      #   or nil if the header is not
+      #   or a symbol if the header is malformed
       #   in the right format
       def parse_header(header)
         match = HEADER_FORMAT.match(header)
 
-        return nil unless match
+        return match[1] if match
 
-        match[1]
+        return fail!(INCORRECT_FORMAT) if HEADER_KEY.match(header)
+
+        nil
       end
     end
   end

app/modules/api/auth_strategies/token.rb
@@ -16,6 +16,10 @@ module Api
       #   but our tests use all sorts of random strings and I don't want to fix every
       #   variant we have....
       HEADER_FORMAT = /Token token="(.+)"/
+      HEADER_KEY = /Token/i
+      INVALID_TOKEN = 'Invalid authentication token'
+      INCORRECT_FORMAT = 'Incorrect token format'
+      EXPIRED_TOKEN = 'Expired authentication token'
 
       def valid?
         !parse_token.nil?
@@ -23,21 +27,24 @@ module Api
 
       def authenticate!
         token = parse_token
-        return if token.nil?
+        return token unless token.is_a?(String)
 
-        return fail!('Invalid authentication token') if token.blank?
+        return fail!(INVALID_TOKEN) if token.blank?
 
-        user = User.find_by(authentication_token: token)
+        user = User.where.not(authentication_token: nil).find_by(authentication_token: token)
 
         # intentionally obscure here, this is a public message.
-        return fail!('Invalid authentication token') if user.nil?
+        return fail!(INVALID_TOKEN) if user.nil?
 
         # Notice how we use Devise.secure_compare to compare the token
         # in the database with the token given in the params, mitigating
         # timing attacks.
         comparison = Devise.secure_compare(user.authentication_token, token)
 
-        return fail!('Invalid authentication token') unless comparison
+        return fail!(INVALID_TOKEN) unless comparison
+
+        # allow access within a rolling window
+        return fail!(EXPIRED_TOKEN) if user.expired_authentication_token?
 
         success! user
       end
@@ -60,14 +67,19 @@ module Api
         request.headers['Authorization']&.to_s
       end
 
-      # @return [String,nil] the bearer token if valid, or nil if the header is not
+      # @return [String,nil,Symbol]
+      #   the bearer token if valid,
+      #   or nil if the header is not
+      #   or a symbol if the header is malformed
       #   in the right format
       def parse_header(header)
         match = HEADER_FORMAT.match(header)
 
-        return nil unless match
+        return match[1] if match
 
-        match[1]
+        return fail!(INCORRECT_FORMAT) if HEADER_KEY.match(header)
+
+        nil
       end
     end
   end

lib/gems/baw-app/lib/initializers/config.rb
@@ -141,6 +141,12 @@ class BawConfigContract < Dry::Validation::Contract
     end
   }
 
+  Authentication = Dry::Schema.define {
+    required(:authentication).hash do
+      required(:token_rolling_expiration).filled(:integer, gt?: 0)
+    end
+  }
+
   DEPRECATIONS = [
     :api,
     :endpoints
@@ -157,7 +163,8 @@ class BawConfigContract < Dry::Validation::Contract
     PathsConfigSchema,
     BatchAnalysisSchema,
     OrganizationNames,
-    AudioEventFileImport
+    AudioEventFileImport,
+    Authentication
   ) do
     required(:trusted_proxies).array(BawApp::Types::IPAddr)
 

lib/gems/baw-app/lib/settings_module.rb
@@ -73,11 +73,22 @@ module BawApp
     end
   end
 
-  # Extensions the Config::Options class and hence also the Settings constant
+  # Strongly typed settings for the authentication section of our config
+  class AuthenticationSettings < ::Dry::Struct
+    # @!attribute [r] token_rolling_expiration
+    #   @return [Integer]
+    attribute :token_rolling_expiration, ::BawApp::Types::Duration
+  end
+
+  # Extensions to the Config::Options class and hence also the Settings constant
   module SettingsModule
     # @return [::BawApp::BatchAnalysisSettings]
     def batch_analysis
       @batch_analysis ||= BatchAnalysisSettings.new(super)
+    end
+
+    def authentication
+      @authentication ||= AuthenticationSettings.new(super)
     end
   end
 end

spec/requests/security/cookies_spec.rb
@@ -1,0 +1,50 @@
+# frozen_string_literal: true
+
+require_relative 'security_context'
+
+describe 'authorization' do
+  include_context 'with security context'
+
+  describe 'cookies' do
+    it 'can sign in and use the cookie to access a resource' do
+      get "/projects/#{project.id}", headers: headers, as: :json
+
+      expect_error(:unauthorized, 'You need to log in or register before continuing.')
+
+      body = {
+        user: {
+          email: reader_user.email,
+          password:
+        }
+      }
+      post '/security', params: body, headers: headers(post: true), as: :json
+
+      expect_success
+      baw_session_cookie
+
+      get "/projects/#{project.id}", headers: headers(cookie: baw_session_cookie_value), as: :json
+
+      expect_success
+    end
+
+    it 'can use a cookie to get session info' do
+      body = {
+        user: {
+          email: reader_user.email,
+          password:
+        }
+      }
+
+      post '/security', params: body, headers: headers(post: true), as: :json
+      assert_session_info_response
+
+      get '/security/user', headers: headers(cookie: baw_session_cookie_value), as: :json
+      assert_session_info_response
+    end
+
+    it 'errors if using an invalid cookie' do
+      get '/security/user', headers: headers(cookie: '_baw_session=invalid'), as: :json
+      expect_error(:unauthorized, 'You need to log in or register before continuing.')
+    end
+  end
+end

commit 0cbfdff2063c3a47b6d096fc6e82595c77b04ad4

Fix bad type reference for workers in baw types
The workers don't necessarily need active support, so they would fail to boot. Now explicitly require active support.

Bug introduced in 1690c672c0f1f5efbde2fefc2de9447185fe299b

config/settings/default.yml
@@ -172,8 +172,8 @@ devise:
 ##secret_token: some long secret token - at least 30 chars, all random
 ##secret_key_base: some long secret token - at least 30 chars, all random
 authentication:
-#   # how long a user can be inactive before their token expires
-#   # Any auth method will refresh the expiration time.
+   # how long a user can be inactive before their token expires
+   # Any auth method will refresh the expiration time.
    token_rolling_expiration: 86400
 paths:
   original_audios:

lib/gems/baw-app/lib/types.rb
@@ -3,6 +3,7 @@
 require 'dry/monads'
 require 'dry-types'
 require 'dry/inflector'
+require 'active_support/duration'
 
 require_relative 'sexagesimal'
 require_relative 'byte_format'
@@ -91,10 +92,10 @@ module BawApp
       ::BawApp::Sexagesimal.parse(value)
     }
 
-    Duration = Constructor(ActiveSupport::Duration) { |value|
+    Duration = Constructor(::ActiveSupport::Duration) { |value|
       next nil if value.blank?
 
-      ActiveSupport::Duration.seconds(value.to_i_strict)
+      ::ActiveSupport::Duration.seconds(value.to_i_strict)
     }
 
     PbsByteFormat = Constructor(Numeric) { |value|