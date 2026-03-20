285266844c504cf73bf2a63e26050c2abcd60c7b
Andrew S <andrew.schwenke@gmail.com>
2026-03-20 05:37:28 +0000

    (spec) improved request spec, working

--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -3,68 +3,72 @@
 describe 'reports/tag_accumulation' do
   create_audio_recordings_hierarchy
 
-  it 'can return a result' do
-    body = { options: { bucket_size: 'day' }, filter: {} }
+  let(:tags) { create_list(:tag, 3, creator: writer_user) }
+  let(:which_tags) { [[0, 1], [0, 0], nil, [2, 2]] }
+  let(:recordings) {
+    which_tags.map do |index|
+      recording = create(:audio_recording, site: site, creator: writer_user)
 
-    # it's not clear you are missing bucket_size from the error message
-    # body = { options: {}, filter: {} }
-    #  {"meta":{"status":422,"message":"Unprocessable Content","error":{"details":"The request could not be understood: param is missing or the value is empty or invalid: options"}},"data":null}
-    # debugger
-    # -- make some tags
-    # build_list(:tag, 3, creator: writer_user)
-    # the key to the setup is multiple recordings and events but the same tags across them.
+      if index
+        tags.fetch_values(*index).each do |tag|
+          create(:audio_event_using_tag, audio_recording: recording, creator: writer_user, tag: tag)
+        end
+      end
 
-    tag_keys = [:koala, :whip_bird, :riflebird]
-    tags = tag_keys.index_with { |tag_name| create(:tag, text: tag_name, creator: writer_user) }
+      recording
+    end
+  }
 
-    # create 2 events + taggings on the default audio_recording from hierarchy
-    event_a_one = create(:audio_event, audio_recording:, creator: writer_user)
-    create(:tagging, audio_event: event_a_one, tag: tags[:koala], creator: writer_user)
+  context('with bucket size of day') do
+    it 'returns the correct buckets and ccounts' do
+      body = {
+        options: { bucket_size: 'day' },
+        filter: {}
+      }
 
-    event_a_two = create(:audio_event, audio_recording:, creator: writer_user)
-    create(:tagging, audio_event: event_a_two, tag: tags[:whip_bird], creator: writer_user)
+      expected = recordings.each_with_index.map { |recording, i|
+        bucket_lower = recording.recorded_date.utc.at_beginning_of_day
+        bucket_upper = bucket_lower + 1.day
+        bucket = "#{bucket_lower}...#{bucket_upper}"
 
-    # -- get dates to make next recordings on
-    # -- with a 1 day gap between them, to confirm the output includes empty buckets
-    second_recording_date = audio_recording.recorded_date + 1.day
-    third_recording_date = audio_recording.recorded_date + 3.days
+        a_hash_including(
+          bucket: match(bucket),
+          cumulative_unique_tag_count: which_tags[0..i].compact.flatten.uniq.size
+        )
+      }
 
-    # -- audio recording 2
-    ar_2 = create(:audio_recording, site: site, creator: writer_user, recorded_date: second_recording_date)
+      post '/reports/tag_accumulation', params: body, **api_headers(writer_token)
+      expect_success
 
-    event_b_one = create(:audio_event, audio_recording: ar_2, creator: writer_user)
-    create(:tagging, audio_event: event_b_one, tag: tags[:koala], creator: writer_user)
-
-    # -- audio recording 3
-    ar_3 = create(:audio_recording, site: site, creator: writer_user, recorded_date: third_recording_date)
-
-    event_c_one = create(:audio_event, audio_recording: ar_3, creator: writer_user)
-    create(:tagging, audio_event: event_c_one, tag: tags[:riflebird], creator: writer_user)
-
-    event_c_two = create(:audio_event, audio_recording: ar_3, creator: writer_user)
-    create(:tagging, audio_event: event_c_two, tag: tags[:riflebird], creator: writer_user)
-
-    post '/reports/tag_accumulation', params: body, **api_headers(writer_token)
-
-    expect_success
-    expected = [a_hash_including(bucket: match(/#{audio_recording.recorded_date.to_date.to_fs(:inspect)}/), cumulative_unique_tag_count: 2),
-                a_hash_including(bucket: match(/2000/), cumulative_unique_tag_count: 2),
-                a_hash_including(bucket: match(/2000/), cumulative_unique_tag_count: 2),
-                a_hash_including(bucket: match(/2000/), cumulative_unique_tag_count: 3)]
-
-    expect(api_data).to match expected
+      expect(api_data).to match expected
+    end
   end
 
-  # first, lets make three recordings/events on three days, use a day bucket, and just get total tag count per bucket
-  # audio_recording.start_time  + 1 day, + 2 days, something like that?
+  context('with bucket size of week') do
+    it 'returns the correct buckets and counts' do
+      body = {
+        options: { bucket_size: 'week' },
+        filter: {}
+      }
 
-  # later on:
-  # create three audio recordings,
-  # day 1, day 2, day 3
-  # create events on each day, with tags
-  # day 1: {tag 1, tag 2}  -- expect tag count = 2, cumulative tag count = 2
-  # day 2: {tag 1}         -- expect tag count = 1, cumulative tag count = 2
-  # day 3: {tag 2, tag 3}  -- expect tag count = 2, cumulative tag count = 3
-  #
-  # it can return the result shape we expect - three buckets
+      # Get week start and the cumulative unique tag count up to that week, for each recording
+      weeks = {}
+      recordings.each_with_index do |recording, i|
+        week_start = recording.recorded_date.utc.at_beginning_of_week(:monday)
+        weeks[week_start] = which_tags[0..i].compact.flatten.uniq.size
+      end
+
+      expected = weeks.map { |week_start, count|
+        a_hash_including(
+          bucket: match("#{week_start}...#{week_start + 1.week}"),
+          cumulative_unique_tag_count: count
+        )
+      }
+
+      post '/reports/tag_accumulation', params: body, **api_headers(writer_token)
+      expect_success
+
+      expect(api_data).to match expected
+    end
+  end
 end
