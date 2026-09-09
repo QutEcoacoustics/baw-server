0fdb29a9091715b96a6557aa428e4c10288b7391
Anthony Truskinger <anthony.truskinger@gmail.com>
2026-09-09 13:20:58 +1000

    Update git-lfs installation method
    Hopefully this routes around the 
    
    `Unable to locate package liblttng-ust0`
    
    error

--- provision/web/dev_setup.sh
+++ provision/web/dev_setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # git-lfs needed for working with dev container (not for prod)
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 DEBIAN_FRONTEND=noninteractive apt-get install git-lfs
 git lfs install
 
